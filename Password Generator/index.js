a9b488b63a63b33dbc6640004571e90e6d9a4ff0
Yukari Yamamoto <ymt.yukari@gmail.com>
2026-06-24 09:23:48 -0400

    Completed Random Password Generator project.
    Added JavaScript functions so users can interact with the button and generate 2 passwords at the same time.

--- Password Generator/index.js
+++ Password Generator/index.js
@@ -1,8 +1,29 @@
 const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
 "/"];
 
+let passwordLength = 12
+let passwordOne = document.getElementById("password-one")
+let passwordTwo = document.getElementById("password-two")
 
+function getRandomCharacter() {
+    let randomChar = Math.floor(Math.random() * characters.length)
+    return characters[randomChar]
+}
 
-function generatePass() {
-    let generatePass() = document.getElementById("generate-btn");
+function generateRandomPassword() {
+    let randomPassword = ""
+    for (let i = 0; i < passwordLength; i++) {
+        randomPassword += getRandomCharacter()
+    }
+    return randomPassword
+}
+
+function generateBtn() {
+    console.log("User clicked to generate 2 passwords.")
+    const generatedPasswordOne = generateRandomPassword()
+    const generatedPasswordTwo = generateRandomPassword()
+    passwordOne.textContent = generatedPasswordOne
+    passwordTwo.textContent = generatedPasswordTwo
+    console.log("Password 1:", generatedPasswordOne)
+    console.log("Password 2:", generatedPasswordTwo)
 }
